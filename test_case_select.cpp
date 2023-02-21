4c1404d9f91079aaa4a30c2a097f59b48c5b6b52
liliang <649743135@qq.com>
2023-02-21 15:47:48 +0800

    test case select

--- test_case_select.cpp
+++ test_case_select.cpp
@@ -3,24 +3,24 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-	char** result;
-	int  row, colum, rc;
+
+	int  rc;
 	sqlite3* pdb;
-	char* message;
 
 	const char* sql = "select mc.id,mc.name,mc.config->> 'type' as customer_type,mc.config,a.version as last_sync_version,a.create_time as last_sync_time from mapper_customer mc join (select mcv.version,mcv.customer_id,mcv.create_time from mapper_customer_version mcv where mcv.version in (select max(version) from mapper_customer_version where customer_id = mcv.customer_id)) a  on a.customer_id = mc.id order by mc.id asc";
 
 	rc = sqlite3_open("test.db", &pdb);
 	if (rc == SQLITE_OK) {
-		rc = sqlite3_get_table(pdb, sql,
-			&result, &row, &colum, &message);
+		sqlite3_stmt* stmt_handler;
+		rc = sqlite3_prepare_v2(pdb, sql, -1, &stmt_handler, NULL);
 		if (rc == SQLITE_OK) {
-			for (int i = 0; i <= row; i++) {
-				for (int j = 0; j < colum; j++) {
-					printf("%s|", *(result + colum * i + j));
-				}
-				printf("\n");
+			while ((rc = sqlite3_step(stmt_handler)) == SQLITE_ROW)
+			{
+				int idType = sqlite3_column_type(stmt_handler, 1);
+				const unsigned char* id = sqlite3_column_text(stmt_handler, 1);
+				printf("%d %s\n", idType, id);
 			}
+			sqlite3_finalize(stmt_handler);
 		}
 		sqlite3_close(pdb);
 	}
