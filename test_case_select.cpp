f9fe41dc477a81d1b6216defabe3add9f90e77e3
liliang <649743135@qq.com>
2023-02-28 16:16:39 +0800

    feat: btree traversal

--- test_case_select.cpp
+++ test_case_select.cpp
@@ -4,21 +4,24 @@
 
 int main(int argc, char** argv) {
 
-	int  rc;
+	int  rc, nCol;
 	sqlite3* pdb;
 
-	const char* sql = "select mc.id,mc.name,mc.config->> 'type' as customer_type,mc.config,a.version as last_sync_version,a.create_time as last_sync_time from mapper_customer mc join (select mcv.version,mcv.customer_id,mcv.create_time from mapper_customer_version mcv where mcv.version in (select max(version) from mapper_customer_version where customer_id = mcv.customer_id)) a  on a.customer_id = mc.id order by mc.id asc";
-
+	const char* sql = "select * from Opcodes where Desc like 'ResultRow'";
 	rc = sqlite3_open("test.db", &pdb);
 	if (rc == SQLITE_OK) {
 		sqlite3_stmt* stmt_handler;
 		rc = sqlite3_prepare_v2(pdb, sql, -1, &stmt_handler, NULL);
 		if (rc == SQLITE_OK) {
+			nCol = sqlite3_column_count(stmt_handler);
 			while ((rc = sqlite3_step(stmt_handler)) == SQLITE_ROW)
 			{
-				int idType = sqlite3_column_type(stmt_handler, 1);
-				const unsigned char* id = sqlite3_column_text(stmt_handler, 1);
-				printf("%d %s\n", idType, id);
+				for (size_t i = 0; i < nCol; i++)
+				{
+					const unsigned char* val = sqlite3_column_text(stmt_handler, i);
+					printf("%s\t", val);
+				}
+				printf("\n");
 			}
 			sqlite3_finalize(stmt_handler);
 		}
